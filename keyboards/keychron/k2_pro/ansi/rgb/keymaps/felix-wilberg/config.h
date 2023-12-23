47a5f87d5d35d993fdcadb130572bc0f025fb0f5
Felix <36933961+felix-wilberg@users.noreply.github.com>
2023-12-23 08:41:13 +0100

    added ignore_mod_type_interrupt and it is way better

---
+++ keyboards/keychron/k2_pro/ansi/rgb/keymaps/felix-wilberg/config.h
@@ -0,0 +1,12 @@
+// Configure the global tapping term (default: 200ms)
+#undef TAPPING_TERM
+#define TAPPING_TERM 210
+
+// Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
+#define QUICK_TAP_TERM 0
+
+// Prevent normal rollover on alphas from accidentally triggering mods.
+// #define HOLD_ON_OTHER_KEY_PRESS 0
+
+// Prevent normal rollover on alphas from accidentally triggering mods.
+#define IGNORE_MOD_TAP_INTERRUPT
